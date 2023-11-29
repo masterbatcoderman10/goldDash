6e19dfa7227ad828b5460522c8e0fb86a2c83eb7
masterbatcoderman10 <suhaib.athar@gmail.com>
2023-11-30 01:32:07 +0400

    updated script

--- db.py
+++ db.py
@@ -1,32 +1,37 @@
+import logging
 from sqlalchemy import Numeric, Date, create_engine, select, insert
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 from dotenv import load_dotenv
 from script import *
 from db_tables import *
 import os
+import time
+
+# Set up logging
+logging.basicConfig(filename='logs.txt', level=logging.INFO)
 
 load_dotenv()
 
 url = os.getenv('PSQL_URL')
-
-date, prices, currency = get_data(init_driver())
-price22k = float(prices[1])
-price24k = float(prices[0])
-price18k = float(prices[2])
-
+date = dt.datetime.now().date()
+logging.info(f'------------------------------------------')
+logging.info(f'DATE: {date}')
+logging.info('\n\n\n')
+start = time.time()
+logging.info('Getting data')
+prices, currency = get_data(init_driver())
+logging.info('Fetched data')
+price22k = prices[1]
+price24k = prices[0]
+price18k = prices[2]
 
 engine = create_engine(url, echo=True)
 
 stmt = select(GoldRates)
 counter = 5
-#Making update
-print('making update')
+# Making update
+logging.info('Making update')
 with Session(engine) as session:
-    # for i, row in enumerate(session.execute(stmt)):
-    #     print(row)
-    #     if i == counter:
-    #         break
-
     todays_gold_rate = GoldRates(
         pricedate=date,
         price22k=price22k,
@@ -36,10 +41,13 @@ with Session(engine) as session:
     
     todays_currency = Currencies(
         date=date,
-        price=float(currency)
+        price=currency
     )
     
     session.add_all([todays_gold_rate, todays_currency])
     session.commit()
 
-
+end = time.time()
+logging.info(f"Script took {end - start} seconds to run")
+logging.info('\n\n\n')
+logging.info('------------------------------------------')
--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,14 +9,20 @@ import re
 import time
 
 def init_driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    service = Service("./chromedriver")
-    driver = webdriver.Chrome(service=service, options=chrome_options) # replace with the path to your chromedriver
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("start-maximized")
+    options.add_argument("enable-automation")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-browser-side-navigation")
+    options.add_argument("--disable-gpu")
+    service = Service("./geckodriver")
+    driver = webdriver.Firefox(service=service, options=options)
     return driver
 
 def get_data(driver):
-    today = dt.datetime.now().date()
+    # today = dt.datetime.now().date()
     driver.get('https://www.khaleejtimes.com/gold-forex') # replace with your website URL
 
     row_nums = [2,3, 5]
@@ -25,16 +31,28 @@ def get_data(driver):
     for row in row_nums:
         x_path = f"(//div[@class = 'draft-rates-main-wrapper-nf maxwidth-67'])[1]/div/table/tbody/tr[{row}]/td[2]"
         element = driver.find_element(by=By.XPATH, value=x_path).text
-        gold_prices.append(element)
+        try:
+            gold_prices.append(float(element))
+        except:
+            x_path = f"(//div[@class = 'draft-rates-main-wrapper-nf maxwidth-67'])[1]/div/table/tbody/tr[{row}]/td[1]"
+            element = driver.find_element(by=By.XPATH, value=x_path).text
+            gold_prices.append(float(element))
+
 
     currency_xpath = "//div[@class = 'draft-rates-main-wrapper-nf']/div[2]/table/tbody/tr[1]/td[2]"
     currency = driver.find_element(by=By.XPATH, value=currency_xpath).text
 
-    currency_val = re.search(r'\d\d.\d\d', currency).group()
+    try:
+        currency_val = float(re.search(r'\d\d.\d\d', currency).group())
+    except:
+        currency_xpath = "//div[@class = 'draft-rates-main-wrapper-nf']/div[2]/table/tbody/tr[1]/td[2]"
+        currency = driver.find_element(by=By.XPATH, value=currency_xpath).text
+        currency_val = float(re.search(r'\d\d.\d\d', currency).group())
+
 
     driver.quit()
 
-    return today, gold_prices, currency_val
+    return gold_prices, currency_val
 
 # start = time.time()
 # print(get_data(init_driver()))
